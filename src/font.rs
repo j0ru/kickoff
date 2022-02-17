03d7f7b1c7229519e2cc661a9737d62a30910640
Folke Gleumes <folke@gleumes.org>
2022-02-17 15:01:14 +0100

    use fontdue layouts to cleanup code and enable fallback fonts

--- src/font.rs
+++ src/font.rs
@@ -1,43 +1,63 @@
 use crate::color::Color;
-
+use fontdue::layout::{GlyphRasterConfig, LayoutSettings, TextStyle, CoordinateSystem, Layout};
+use fontdue::Metrics;
+use std::cell::RefCell;
 use std::collections::HashMap;
-use std::io::Read;
-use std::fs::File;
+use std::path::PathBuf;
+
+use tokio::{fs::File, io::{self, AsyncReadExt}};
 
 use fontconfig::Fontconfig;
-
-use fontdue::{layout::GlyphRasterConfig, FontSettings, Metrics};
 
 use image::{Pixel, RgbaImage};
 
 pub struct Font {
-    font: fontdue::Font,
+    fonts: Vec<fontdue::Font>,
+    layout: RefCell<Layout>,
     scale: f32,
-    glyph_cache: HashMap<GlyphRasterConfig, (Metrics, Vec<u8>)>,
+    glyph_cache: RefCell<HashMap<GlyphRasterConfig, (Metrics, Vec<u8>)>>,
 }
 
 impl Font {
-    pub fn new(name: &str, size: f32) -> Font {
+    pub async fn new(font_names: Vec<String>, size: f32) -> io::Result<Font> {
         let fc = Fontconfig::new().expect("Couldn't load fontconfig");
-        let font_path = fc.find(name, None).expect("Couldn't find font").path;
-        let mut font_file = File::open(font_path.to_str().unwrap()).expect("Couldn't load font file");
-        let mut font_buffer = Vec::new();
-        font_file.read_to_end(&mut font_buffer).expect("Couldn't load font file");
-        Font {
-            font: fontdue::Font::from_bytes(font_buffer, FontSettings::default())
-                .expect("Failed to parse Font"),
-            scale: size,
-            glyph_cache: HashMap::new(),
+        let font_paths: Vec<PathBuf> = vec![font_names
+            .iter()
+            .map(|name| fc.find(name, None).unwrap().path)
+            .collect()];
+        let mut font_data = Vec::new();
+
+        for font_path in font_paths {
+            let mut font_buffer = Vec::new();
+            File::open(font_path.to_str().unwrap())
+                .await?
+                .read_to_end(&mut font_buffer)
+                .await?;
+            font_data.push(
+                fontdue::Font::from_bytes(font_buffer, fontdue::FontSettings::default()).unwrap(),
+            );
         }
+
+        Ok(Font {
+            fonts: font_data,
+            layout: RefCell::new(Layout::new(CoordinateSystem::PositiveYDown)),
+            scale: size,
+            glyph_cache: RefCell::new(HashMap::new()),
+        })
     }
 
-    fn render_glyph(&mut self, conf: GlyphRasterConfig) -> (Metrics, Vec<u8>) {
-        if let Some(bitmap) = self.glyph_cache.get(&conf) {
+    fn render_glyph(&self, conf: GlyphRasterConfig) -> (Metrics, Vec<u8>) {
+        let mut glyph_cache = self.glyph_cache.borrow_mut();
+        if let Some(bitmap) = glyph_cache.get(&conf) {
             bitmap.clone()
         } else {
-            self.glyph_cache
-                .insert(conf, self.font.rasterize_config(conf));
-            self.glyph_cache.get(&conf).unwrap().clone()
+            let font: Vec<&fontdue::Font> = self
+                .fonts
+                .iter()
+                .filter(|f| (*f).file_hash() == conf.font_hash)
+                .collect();
+            glyph_cache.insert(conf, font.first().unwrap().rasterize_config(conf));
+            glyph_cache.get(&conf).unwrap().clone()
         }
     }
 
@@ -49,34 +69,28 @@ impl Font {
         x_offset: u32,
         y_offset: u32,
     ) -> (u32, u32) {
-        let mut width = 0.;
-        for letter in text.chars() {
-            let (meta, bitmap) = self.render_glyph(GlyphRasterConfig {
-                glyph_index: self.font.lookup_glyph_index(letter) as u16,
-                px: self.scale,
-                font_index: 0,
-            });
+        let mut width = 0;
+        let mut layout = self.layout.borrow_mut();
+        layout.reset(&LayoutSettings::default());
+        layout.append(&self.fonts, &TextStyle::new(text, self.scale, 0));
+
+        for glyph in layout.glyphs() {
+            let (_, bitmap) = self.render_glyph(glyph.key);
             for (i, alpha) in bitmap.iter().enumerate() {
                 if alpha != &0 {
-                    let x = ((i % meta.width) as f32 + width + x_offset as f32 + meta.xmin as f32)
-                        as u32;
-                    let y = ((i as f32 / meta.width as f32) + y_offset as f32 + self.scale
-                        - meta.height as f32
-                        - meta.ymin as f32) as u32;
-                    if x < image.width() && y < image.height() {
-                        if alpha == &255 {
-                            image.put_pixel(x, y, image::Rgba([color.0, color.1, color.2, 255]));
-                        } else {
-                            image
-                                .get_pixel_mut(x, y)
-                                .blend(&image::Rgba([color.0, color.1, color.2, *alpha]));
-                        }
-                    }
+                    let x = glyph.x + x_offset as f32 + (i % glyph.width) as f32;
+                    let y = glyph.y + y_offset as f32 + (i / glyph.width) as f32;
+
+                    image
+                        .get_pixel_mut(x as u32, y as u32)
+                        .blend(&image::Rgba([color.0, color.1, color.2, *alpha]));
                 }
             }
-            width += meta.advance_width;
+        }
+        if let Some(glyph) = layout.glyphs().last() {
+            width = glyph.x as usize + glyph.width;
         }
 
-        (width as u32, self.scale as u32)
+        (width as u32, layout.height() as u32)
     }
 }

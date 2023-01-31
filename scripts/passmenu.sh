424a720a8952b951d487a6317ba79c87f3a43664
j0ru <folke@gleumes.org>
2023-01-31 23:27:53 +0100

    add example for stdin/out

---
+++ scripts/passmenu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+shopt -s nullglob globstar
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password_files=( "$prefix"/**/*.gpg )
+password_files=( "${password_files[@]#"$prefix"/}" )
+password_files=( "${password_files[@]%.gpg}" )
+
+password=$(printf '%s\n' "${password_files[@]}" | kickoff --stdout --from-stdin)
+
+pass -c "$password"

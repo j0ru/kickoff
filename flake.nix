37405cb97d05617535af84f81b4669b94dc89d4c
Folke H. Gleumes <folke@gleumes.org>
2026-01-27 15:49:59 +0100

    feat: add nix flake (#218)
    Co-authored-by: Folke Gleumes <folke.gleumes@contabo.de>

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      buildInput = with pkgs; [
+        cargo
+        rustc
+        rustfmt
+        clippy
+        rust-analyzer
+        fontconfig
+        libxkbcommon
+      ];
+      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+    };
+    packages.x86_64-linux.default = pkgs.callPackage ./default.nix {};
+  };
+}

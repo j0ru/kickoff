9aabd8f9e3cc0fa28fe16123745251b63c38829b
Folke Gleumes <folke-git@gleumes.org>
2026-01-27 15:56:38 +0100

    fix: add description for flake

--- flake.nix
+++ flake.nix
@@ -1,28 +1,31 @@
 {
-  description = "A very basic flake";
+  description = "Kickoff, a minimalistic program launcher";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  in {
-    devShells."x86_64-linux".default = pkgs.mkShell {
-      buildInput = with pkgs; [
-        cargo
-        rustc
-        rustfmt
-        clippy
-        rust-analyzer
-        fontconfig
-        libxkbcommon
-      ];
-      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        buildInput = with pkgs; [
+          cargo
+          rustc
+          rustfmt
+          clippy
+          rust-analyzer
+          fontconfig
+          libxkbcommon
+        ];
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+      packages.x86_64-linux.default = pkgs.callPackage ./default.nix { };
     };
-    packages.x86_64-linux.default = pkgs.callPackage ./default.nix {};
-  };
 }
